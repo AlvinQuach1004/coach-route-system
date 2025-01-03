790091cd4b3b4504ac0bd57db4638c26b1400b76
AlvinQuach1004 <alvin.quach.goldenowl@gmail.com>
2025-01-03 21:37:42 +0700

    change quote in application/index

--- app/frontend/controllers/application/index.js
+++ app/frontend/controllers/application/index.js
@@ -1,27 +1,25 @@
-import { registerControllers } from 'stimulus-vite-helpers';
-import { Application } from '@hotwired/stimulus';
-import "flatpickr/dist/flatpickr.min.css";  // Import Flatpickr CSS
-import flatpickr from "flatpickr";  // Import Flatpickr JavaScript
+import { registerControllers } from 'stimulus-vite-helpers'
+import { Application } from '@hotwired/stimulus'
+import 'flatpickr/dist/flatpickr.min.css' // Import Flatpickr CSS
+import flatpickr from 'flatpickr' // Import Flatpickr JavaScript
 
-
-const application = Application.start();
+const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false;
-window.Stimulus = application;
+application.debug = false
+window.Stimulus = application
 
-const controllers = import.meta.glob('./**/*_controller.js', { eager: true });
+const controllers = import.meta.glob('./**/*_controller.js', { eager: true })
 
 // Register Stimulus controllers
-registerControllers(application, controllers);
+registerControllers(application, controllers)
 
 document.addEventListener('DOMContentLoaded', function () {
-    flatpickr("#datepicker", {
-      dateFormat: "d-m-Y", 
-      minDate: "today", 
-      mode: "single",    
-      static: true,
-      theme: "light", 
-    });
-  });
-  
+  flatpickr('#datepicker', {
+    dateFormat: 'd-m-Y',
+    minDate: 'today',
+    mode: 'single',
+    static: true,
+    theme: 'light',
+  })
+})
