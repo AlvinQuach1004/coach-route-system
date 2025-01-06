62d5852867e44ad0e2bd00a8ca03ec7d89e8d3bf
AlvinQuach1004 <alvin.quach.goldenowl@gmail.com>
2025-01-06 16:03:55 +0700

    config ticket_spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  it 'creates a valid ticket with associated booking and schedule' do
-    ticket = create(:ticket)
-    expect(ticket).to be_valid
-    expect(ticket.booking).to be_present
-    expect(ticket.schedule).to be_present
-    expect(ticket.price.to_f).to be_a(Float)
-    expect(ticket.seat_number).to be_a(String)
-    expect(['booked', 'cancelled', 'paid']).to include(ticket.status)
+  describe 'validations and associations' do
+    it 'creates a valid ticket with associated booking and schedule' do
+      ticket = create(:ticket)
+      expect(ticket).to be_valid
+      expect(ticket.booking).to be_present
+      expect(ticket.schedule).to be_present
+      expect(ticket.price.to_f).to be_a(Float)
+      expect(ticket.seat_number).to be_a(String)
+      expect(['booked', 'paid', 'cancelled']).to include(ticket.status)
+    end
   end
 end
+
