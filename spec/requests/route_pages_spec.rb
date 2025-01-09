54cf698b12d94ca99f606b02a67587a2781e1b69
AlvinQuach1004 <alvin.quach.goldenowl@gmail.com>
2025-01-09 16:11:58 +0700

    fix spec for test route

--- spec/requests/route_pages_spec.rb
+++ spec/requests/route_pages_spec.rb
@@ -1,10 +1,1 @@
 require 'rails_helper'
-
-RSpec.describe 'RoutePages', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/route_pages/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-end
