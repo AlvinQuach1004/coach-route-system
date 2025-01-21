e87464db707b63bf79c9472892b95251a04f31f3
AlvinQuach1004 <alvin.quach.goldenowl@gmail.com>
2025-01-21 12:16:50 +0700

    use prettier for format js code

--- app/frontend/controllers/shared/index.js
+++ app/frontend/controllers/shared/index.js
@@ -1,7 +1,7 @@
 import { registerControllers } from 'stimulus-vite-helpers';
 import { Application } from '@hotwired/stimulus';
 import './toast';
-import './modal'
+import './modal';
 
 const application = Application.start();
 
